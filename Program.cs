15d31608903cc3ddb6d13395fb7053b14e1b9d46
petrus <pshai671@gmail.com>
2023-02-06 11:43:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using object_oriented_program_of_area_of_different_shapes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Drawing circle = new Circle();
+        Drawing square = new Square();
+        Drawing rectangle = new Rectangle();
+
+
+        Console.WriteLine("Area :" + circle.Area());
+        Console.WriteLine("Area :" + square.Area());
+        Console.WriteLine("Area :" + rectangle.Area());
+    }
+}
